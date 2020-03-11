a3ec80aad61e7f3ee2c65f2076fd8b34a6ff439f
Soham Patil <mr.soham143@gmail.com>
2020-03-11 22:11:26 +0530

    completed N queen problem

--- Bit Manipulation/Nqueen probelm.cpp
+++ Bit Manipulation/Nqueen probelm.cpp
@@ -9,9 +9,26 @@ typedef long long int ll;
 
 int ans = 0;
 int done;
+
 void solve(int rowmask, int ld, int rd)
 {
+	if(rowmask == done)
+	{
+		ans++;
+		return;
+	}
 
+	int avail = done & (~(rowmask|ld|rd)) ;
+
+	while(avail > 0)
+	{
+		int pp = avail & (-avail);
+		avail = avail - pp;
+
+		solve(rowmask|pp, (ld|pp)<<1, (rd|pp)>>1);
+
+	}	 
+	 
 
 }
 
@@ -19,7 +36,11 @@ int main()
 {
 	int n;
 	cin>>n;
-	
-	solve(0,0,0);
+	done = (1<<n)-1;
+
+	solve(0,0,0);  //one for rpw asking , one for left diiagonal(ld),
+	//rd = for right diagonal
+	cout<<"total possible ways : "<<ans<<endl;
+
 	return 0;
 }
