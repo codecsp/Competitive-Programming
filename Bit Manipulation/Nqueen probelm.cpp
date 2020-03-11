062236f32e13d56120b28d26d3335f4392a770df
codecsp <mr.soham143@gmail.com>
2020-03-11 17:15:39 +0530

    N queen problem

---
+++ Bit Manipulation/Nqueen probelm.cpp
@@ -0,0 +1,25 @@
+/*
+ N queen problems
+  Backtracking + Bitmmasking  (Most efiicient approach)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int ans = 0;
+int done;
+void solve(int rowmask, int ld, int rd)
+{
+
+
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	
+	solve(0,0,0);
+	return 0;
+}
