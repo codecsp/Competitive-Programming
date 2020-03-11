69c8b313c00fc838ba1ec9a549baa22b33c9ff56
codecsp <mr.soham143@gmail.com>
2020-03-11 10:28:44 +0530

    added inclusion principle

--- Bit Manipulation/inclusion_exclusion.cpp
+++ Bit Manipulation/inclusion_exclusion.cpp
@@ -1,6 +1,14 @@
 /*
 	Find the number of numbers less than n, that are divisible by all primes less than 20 
 
+	solution idea : finding all subsets of the given raange of primes and operating bitwise
+
+	like n(AUB) = n(A) + n(B) - n(A and B)
+
+	n(A and  B )  =  n/(A*B);
+
+	provided A,B should be prime.
+	If they are not prime then you have to find LCM of numbers.
 */
 
 #include<bits/stdc++.h>
@@ -12,13 +20,13 @@ int main()
 
 	ll n;
 	cin>>n;
-	ll primes[]  = {2,3,5,7,11,13,17,19};
+	ll primes[]  = {2,3,5,7,11,13,17,19}; 
 
 	ll primeCount = 8;
 
 	ll ans = 0;
 	
-	for(ll i=1;i<= (1<<primeCount)-1 ;i++)
+	for(ll i=1;i<= (1<<primeCount)-1 ;i++)  // finding subsets of the primes array
 	{
 		ll denom = 1;			//cout<<"inf loop\n";
 
